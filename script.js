0f4ecf71b08aa47c1df8142bc8f834e2fe393e43
ErikAbrahamson <e.a.abrahamson@gmail.com>
2015-07-10 16:17:01 -0600

    Solution for js loops

---
+++ script.js
@@ -0,0 +1,32 @@
+//1
+var problemOne = for (var i = 0; i <= 1000; i +=100) {
+  console.log(i);
+}
+//2
+for (var i = 1; i <= 128; i *=i) {
+  console.log(i);
+}
+//3
+for (var i = 0; i <= 10; i +=2) {
+  console.log(i);
+}
+//4
+for (var i = 0; i <= 15; i +=3) {
+  console.log(i);
+}
+//5
+for (var i = 9; i >= 0; i--) {
+  console.log(i);
+}
+//6
+for (var i = 1; i <= 4; i++) {
+  console.log(i);
+  console.log(i);
+  console.log(i);
+}
+//7
+for (var i = 1; i <= 3; i++) {
+  for (var a = 0; a <= 4; a++) {
+    console.log(a);
+  }
+}
